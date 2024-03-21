e724242ebe49a2337531640ff78012175d07ba54
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-03-21 13:36:11 +0200

    Create page.tsx

---
+++ app/taikatassut/page.tsx
@@ -0,0 +1,9 @@
+export default function Taikatassut() {
+  return (
+    <div>
+      <h1>Turun Taikatassut</h1>
+      <p>Turun Taikatassut on yksi Varsinais-Suomen Kaverikoirat ry:n alaosastoista. Taikatassut toimivat Turun alueella ja heidän toimintaansa kuuluu muun muassa vierailut vanhainkodeissa ja sairaaloissa. Taikatassut järjestävät myös erilaisia tapahtumia ja koulutuksia.</p>
+      <p>Taikatassut on perustettu vuonna 2011 ja heillä on tällä hetkellä noin 30 koirakkoa. Taikatassujen koirakot ovat monenlaisia rotuja ja kokoisia, mutta kaikilla on yhteinen intohimo auttaa muita ihmisiä koiriensa kanssa.</p>
+    </div>
+  );
+}
