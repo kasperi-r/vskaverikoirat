eda69d609d15ae5d9c844af4ba9fc4d476144d70
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-03-21 13:35:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "./ui/header";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <div className="sm:container sm:mx-auto px-4 mt-4">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
