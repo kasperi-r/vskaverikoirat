9be7eab52a210eb5de4cceb1bfd3611b226db6a5
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-03-21 14:52:53 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
+import dogs from "../public/dogs.jpg";
 
 export default function Home() {
   return (
     <main>
+      <div>
+        <h1 className="mt-6 mb-6">Varsinais-Suomen Kaverikoirat</h1>
+        <Image
+          src={dogs}
+          alt="Kaverikoirat"
+        />
+      </div>
     </main>
   );
 }
