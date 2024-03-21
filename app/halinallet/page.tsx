01378499d66c6cbedaba633adcb6de6a0eea18f3
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-03-21 14:41:55 +0200

    Create page.tsx

---
+++ app/halinallet/page.tsx
@@ -0,0 +1,9 @@
+export default function Halinallet() {
+  return (
+    <div>
+      <h1>Pohjoisen Turun Halinallet</h1>
+      <p>Pohjoisen Turun Halinallet on yksi Varsinais-Suomen Kaverikoirat ry:n alaosastoista. Halinallet toimivat Turun pohjoisella alueella ja heidän toimintaansa kuuluu muun muassa vierailut vanhainkodeissa ja sairaaloissa. Halinallet järjestävät myös erilaisia tapahtumia ja koulutuksia.</p>
+      <p>Halinallet on perustettu vuonna 2015 ja heillä on tällä hetkellä noin 20 koirakkoa. Halinallet koirakot ovat monenlaisia rotuja ja kokoisia, mutta kaikilla on yhteinen intohimo auttaa muita ihmisiä koiriensa kanssa.</p>
+    </div>
+  );
+}
