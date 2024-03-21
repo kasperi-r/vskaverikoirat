2cfe5ff7771f4b77e11062055fc0310f8452b824
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-03-21 14:52:48 +0200

    Create page.tsx

---
+++ app/karvatassut/page.tsx
@@ -0,0 +1,9 @@
+export default function Karvatassut() {
+  return (
+    <div>
+      <h1>Salon Karvatassut</h1>
+      <p>Salon Karvatassut on yksi Varsinais-Suomen Kaverikoirat ry:n alaosastoista. Karvatassut toimivat Salon alueella ja heidän toimintaansa kuuluu muun muassa vierailut vanhainkodeissa ja sairaaloissa. Karvatassut järjestävät myös erilaisia tapahtumia ja koulutuksia.</p>
+      <p>Karvatassut on perustettu vuonna 2010 ja heillä on tällä hetkellä noin 15 koirakkoa. Karvatassujen koirakot ovat monenlaisia rotuja ja kokoisia, mutta kaikilla on yhteinen intohimo auttaa muita ihmisiä koiriensa kanssa.</p>
+    </div>
+  );
+}
