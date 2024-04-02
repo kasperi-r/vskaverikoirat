611b52376ffa35401503fd06674f714f45ed842f
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-04-02 19:15:44 +0300

    Update index.tsx

--- app/ui/header/index.tsx
+++ app/ui/header/index.tsx
@@ -11,7 +11,7 @@ export default function Header() {
 
   return (
     <header className="w-full p-4 px-5 sm:px-10 flex items-center justify-between border-0 border-b-orange-400">
-      <h4 className="">Varsinais-Suomen Kaverikoirat</h4>
+      <h4 className="text-xl">Varsinais-Suomen Kaverikoirat</h4>
       <nav className="w-max sm:flex fixed right-1/2 translate-x-1/2">
         {links.map(({ href, label }) => (
           <Link key={label} className="md:hover:text-orange-400 mx-4" href={href}>{label}</Link>
