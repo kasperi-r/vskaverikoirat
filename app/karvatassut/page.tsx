178976c0062fb42b7731bde43dbeaf681d55fab5
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-04-03 16:46:00 +0300

    Refactor info pages to use InfoComponent

--- app/karvatassut/page.tsx
+++ app/karvatassut/page.tsx
@@ -1,9 +1,13 @@
+import InfoComponent from "../ui/InfoComponent";
+
 export default function Karvatassut() {
   return (
-    <div>
-      <h1>Salon Karvatassut</h1>
-      <p>Salon Karvatassut on yksi Varsinais-Suomen Kaverikoirat ry:n alaosastoista. Karvatassut toimivat Salon alueella ja heidän toimintaansa kuuluu muun muassa vierailut vanhainkodeissa ja sairaaloissa. Karvatassut järjestävät myös erilaisia tapahtumia ja koulutuksia.</p>
-      <p>Karvatassut on perustettu vuonna 2010 ja heillä on tällä hetkellä noin 15 koirakkoa. Karvatassujen koirakot ovat monenlaisia rotuja ja kokoisia, mutta kaikilla on yhteinen intohimo auttaa muita ihmisiä koiriensa kanssa.</p>
-    </div>
+    <InfoComponent
+      title="Salon Karvatassut"
+      body={[
+        "Salon Karvatassut on yksi Varsinais-Suomen Kaverikoirat ry:n alaosastoista. Karvatassut toimivat Salon alueella ja heidän toimintaansa kuuluu muun muassa vierailut vanhainkodeissa ja sairaaloissa. Karvatassut järjestävät myös erilaisia tapahtumia ja koulutuksia.",
+        "Karvatassut on perustettu vuonna 2010 ja heillä on tällä hetkellä noin 15 koirakkoa. Karvatassujen koirakot ovat monenlaisia rotuja ja kokoisia, mutta kaikilla on yhteinen intohimo auttaa muita ihmisiä koiriensa kanssa.",
+      ]}
+    />
   );
 }
