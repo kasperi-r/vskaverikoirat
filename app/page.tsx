226881baafc1befdf0379dd483482d51ab12c5e2
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-04-05 15:59:41 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,41 +18,37 @@ export default async function Home() {
   const blogPosts = await fetchBlogPosts({ preview: draftMode().isEnabled })
 
   return (
-    <main>
-      <div className="">
-        <div className=''>
-          <div className=''>
-            <ul>
-              {blogPosts.map((blogPost: BlogPost) => {
-                console.log(blogPost)
-                return (
-                  <li className="mb-12 pb-12 border-b-2" key={blogPost.title}>
-                    {/* Render the blog post image */}
-                    {blogPost.image && (
-                      // Use the Contentful Images API to render
-                      // responsive images. No next/image required:
-                      <img
-                        src={blogPost.image.src}
-                        alt={blogPost.image.alt}
-                      />
-                    )}
+    <div className="">
+      <main className="">
+        <ul>
+          {blogPosts.map((blogPost: BlogPost) => {
+            console.log(blogPost)
+            return (
+              <li className="mb-12 pb-12 border-b-2" key={blogPost.title}>
+                {/* Render the blog post image */}
+                {blogPost.image && (
+                  // Use the Contentful Images API to render
+                  // responsive images. No next/image required:
+                  <img
+                    src={blogPost.image.src}
+                    alt={blogPost.image.alt}
+                  />
+                )}
 
-                    {/* Render the blog post title */}
-                    <h1 className='my-6 text-4xl font-sans font-semibold leading-[1.3]'>{blogPost.title}</h1>
+                {/* Render the blog post title */}
+                <h1 className='my-6 text-4xl font-sans font-semibold leading-[1.3]'>{blogPost.title}</h1>
 
-                    <p className='mb-6 font-sans font-light'>{formatDate(blogPost.createdAt)}</p>
+                <p className='mb-6 font-sans font-light'>{formatDate(blogPost.createdAt)}</p>
 
-                    {/* Render the blog post body */}
-                    <div className='block text-base font-light leading-relaxed'>
-                      <RichText document={blogPost.body} />
-                    </div>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </main>
+                {/* Render the blog post body */}
+                <div className='block text-base font-light leading-relaxed'>
+                  <RichText document={blogPost.body} />
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
+    </div>
   );
 }
