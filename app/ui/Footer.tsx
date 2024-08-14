422a82077e7ddc39880535d608a90f1d5bdf2935
Kasperi Rautio <kasperi.h.rautio@utu.fi>
2024-08-14 11:18:07 +0300

    Update icons

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto md:w-[40%] flex flex-col md:flex-row items-center justify-between">
+      <div className="container mx-auto xl:max-w-screen-lg flex flex-col sm:flex-row items-center justify-between">
         <p>&copy; 2024 Varsinais-Suomen Kaverikoirat</p>
         <Image src="/kaverikoira_pysty_orig_web.jpg" alt="Kennelliiton Kaverikoira-logo" width={385} height={210} />
       </div>
--- app/ui/header/index.tsx
+++ app/ui/header/index.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { useState, useRef, useCallback } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import { palanquin } from '../fonts';
-import { FaBars, FaTimes } from 'react-icons/fa';
 import { usePathname } from 'next/navigation';
 
 const links = [
@@ -19,14 +18,10 @@ export default function Header() {
 
   const pathname = usePathname();
 
-  const modalRef = useRef<HTMLDivElement>(null);
-
-  const toggleModal = useCallback(() => {
-    setShowModal(prevShowModal => !prevShowModal);
-  }, []);
+  const toggleModal = () => setShowModal(!showModal);
 
   return (
-    <header className="fixed top-0 z-50 w-full p-4 px-5 sm:px-10 flex items-center justify-between text-white bg-gray-800">
+    <header className="fixed top-0 w-full p-4 px-5 sm:px-10 flex items-center justify-between text-white bg-gray-800">
       <h4 className={`${palanquin.className} text-xl`}>Varsinais-Suomen Kaverikoirat</h4>
       <div className="hidden lg:flex items-center space-y-4 lg:space-y-0 lg:space-x-6 w-full lg:w-auto lg:ml-8">
         {links.slice(0, 4).map(({ path, label }) => (
@@ -38,14 +33,20 @@ export default function Header() {
           <Link key={label} href={path} className={`${path === pathname && "text-orange-300 border-b-2 border-orange-300"} hover:text-orange-300 transition-all`}>{label}</Link>
         ))}
       </div>
-      <button onClick={toggleModal} className="lg:hidden h-4 w-4 flex items-center justify-center">
-        <FaBars />
+      <button onClick={toggleModal}
+        className="lg:hidden">
+        <svg className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <line x1="3" y1="12" x2="21" y2="12" />
+          <line x1="3" y1="6" x2="21" y2="6" />
+          <line x1="3" y1="18" x2="21" y2="18" />
+        </svg>
       </button>
-      <div ref={modalRef}
-        className={`fixed top-20 right-0 bg-gray-800 bg-opacity-95 z-50 flex flex-col items-center justify-center p-4 transition-transform duration-200 ${showModal ? 'translate-x-0' : 'translate-x-full'}`}
-      >
-        <button onClick={toggleModal} className="absolute top-2 right-2 p-2">
-          <FaTimes />
+      <div className={`fixed top-20 right-0 bg-gray-800 bg-opacity-95 z-50 flex flex-col items-center justify-center py-4 px-8 transition-transform duration-200 ${showModal ? 'translate-x-0' : 'translate-x-full'}`}>
+        <button onClick={toggleModal} className="absolute top-1 right-1 p-2">
+          <svg className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
         </button>
         {links.map(({ path, label }) => (
           <Link key={label} href={path} onClick={toggleModal} className={`${path === pathname && "text-orange-300 border-b-2 border-orange-300"} hover:text-orange-300 text-2xl mb-4`}>
