c64151abf87b4d0b3614f718c5e1374214163179
Kasperi Rautio <43678840+kasperi-r@users.noreply.github.com>
2024-08-19 19:17:03 +0300

    Update page.tsx
    Fixed e-mail address

--- app/taikatassut/page.tsx
+++ app/taikatassut/page.tsx
@@ -10,7 +10,7 @@ export default function Taikatassut() {
         <p>Vierailemme vanhusten, vammaisten, nuorten ja lasten luona erilaisissa asumispalveluyksiköissä pääosin Turun seudulla. Yksittäistapahtumiin kysyntää on paljon ja kaikkiin emme pysty vastaamaan, mutta parhaamme yritämme ja usein vierailu onnistuu aikatauluja suunnitellen ja muokaten.</p>
         <p>Toivomme, että elokuun kurssilta saamme paljon innokkaita uusia taikatassulaisia.</p>
         <p>Ryhmänvetäjänä toimii Heli-Maarit Makkonen ja minut tavoittaa:</p>
-        <p>kaverikoirat.taikatassut@gmail.com tai 050 5763081</p>
+        <p>kaverikoira.taikatassut@gmail.com tai 050 5763081</p>
       </div>
       <div className="columns-1 md:columns-2 gap-4 mb-8">
         <Image src="/taikatassut1.jpg" alt="Taikatassut_kuva1" width={1080} height={712} className="w-full mb-4" />
