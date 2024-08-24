173ad1569f6b3f8957656cdd720f6360df211a70
Kasperi Rautio <khraut@utu.fi>
2024-08-24 20:44:31 +0300

    Update mobile menu

--- app/ui/header/index.tsx
+++ app/ui/header/index.tsx
@@ -20,7 +20,7 @@ export default function Header() {
   const toggleModal = () => setShowModal(!showModal);
 
   return (
-    <header className="fixed top-0 w-full p-4 px-5 sm:px-10 flex items-center justify-between text-white bg-gray-800 opacity-90">
+    <header className="fixed top-0 w-full p-4 px-5 sm:px-10 flex items-center justify-between text-white bg-gray-800 opacity-95">
       <Link key={'home'} href={'/'} className={`${palanquin.className} text-xl`}>Varsinais-Suomen Kaverikoirat</Link>
       <div className="hidden lg:flex items-center space-y-4 lg:space-y-0 lg:space-x-6 w-full lg:w-auto lg:ml-8">
         {links.slice(0, 3).map(({ path, label }) => (
@@ -49,19 +49,23 @@ export default function Header() {
                     }`} >
     </span> 
       </button>
-      <div className={`fixed top-0 left-0 w-full h-[400px] bg-opacity-95 z-50 flex flex-col items-center justify-end transition-transform duration-200 ${showModal ? 'translate-y-10' : '-translate-y-full'}`} style={{ backgroundImage: 'url(/paw.svg)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
-        {/* <button onClick={toggleModal} className="absolute top-1 right-1 p-2">
-          <svg className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <line x1="18" y1="6" x2="6" y2="18" />
-            <line x1="6" y1="6" x2="18" y2="18" />
-          </svg>
-        </button> */}
+      <div className={`fixed top-0 left-0 w-full h-[400px] z-50 flex flex-col items-center justify-end transition-transform duration-200 ${showModal ? 'translate-y-10' : '-translate-y-full'}`}
+        style={{ backgroundImage: 'url(/paw.svg)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
         <div className="flex flex-col items-center pb-8 mb-8">
-        {links.map(({ path, label }) => (
-          <Link key={label} href={path} onClick={toggleModal} className={`${path === pathname && "text-orange-300 border-b-2 border-orange-300"} hover:text-orange-300 text-sm mb-1`}>
+        {links.slice(0, 3).map(({ path, label }) => (
+          <Link key={label} href={path} onClick={toggleModal} className={`${path === pathname && "text-orange-300 border-b-2 border-orange-300"} hover:text-orange-300 text-xl mb-1`}>
             {label}
           </Link>
         ))}
+        </div>
+      </div>
+      <div className={`fixed bottom-0 left-0 w-full h-[200px] bg-gray-800 z-50 flex flex-col items-center justify-center transition-transform duration-200 ${showModal ? 'translate-y-0' : 'translate-y-full'}`}>
+        <div className="flex flex-col items-center">
+          {links.slice(3).map(({ path, label }) => (
+            <Link key={label} href={path} onClick={toggleModal} className={`${path === pathname && "text-orange-300 border-b-2 border-orange-300"} hover:text-orange-300 text-xl mb-1`}>
+              {label}
+            </Link>
+          ))}
         </div>
       </div>
     </header>
