a63f83a69ff535e184746f24286e47d938328923
Kasperi Rautio <khraut@utu.fi>
2025-04-07 15:26:01 +0300

    Update Footer.tsx

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -2,13 +2,12 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="mt-auto">
-      <div className="mx-auto px-6 lg:px-0 max-w-screen-lg flex flex-col sm:flex-row items-center justify-between py-4 border-t border-gray-300">
+    <footer>
+      <div className="flex flex-col mx-auto px-6 py-4 max-w-screen-lg sm:flex-row items-center justify-between">
         <p>&copy; 2025 Varsinais-Suomen Kaverikoirat</p>
         <Image
           src="/kaverikoira_pysty_orig_web.jpg"
           alt="Kennelliiton Kaverikoira-logo"
-          className="border-1 border-gray-300"
           width={385}
           height={210}
         />
