c9b7a719da50bcc75539ce27e4b734dad9fadd1f
Kasperi Rautio <khraut@utu.fi>
2025-07-05 16:15:43 +0300

    Update page.tsx

--- app/taikatassut/page.tsx
+++ app/taikatassut/page.tsx
@@ -40,7 +40,7 @@ export default function Taikatassut() {
         <h1 className="text-3xl mb-8">Turun Taikatassut</h1>
         <p>
           Taikatassut -ryhmässä on tällä hetkellä noin 20 koirakkoa ja
-          vakiokohteita meillä on 8 kuukaudessa. Lisäksi teemme
+          vakiokohteita meillä on 6 kuukaudessa. Lisäksi teemme
           yksittäisvierailuja.
         </p>
         <p>
@@ -49,10 +49,6 @@ export default function Taikatassut() {
           kysyntää on paljon ja kaikkiin emme pysty vastaamaan, mutta parhaamme
           yritämme ja usein vierailu onnistuu aikatauluja suunnitellen ja
           muokaten.
-        </p>
-        <p>
-          Toivomme, että elokuun kurssilta saamme paljon innokkaita uusia
-          taikatassulaisia.
         </p>
         <p>Ryhmänvetäjänä toimii Heli-Maarit Makkonen ja minut tavoittaa:</p>
         <p>kaverikoira.taikatassut@gmail.com tai 050 5763081</p>
